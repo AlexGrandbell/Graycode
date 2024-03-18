e62da9b03e3e26a494be2b1a4e235f0aae124f6c
AlexJR <125436791+AlexGrandbell@users.noreply.github.com>
2024-03-18 12:07:19 +0800

    更改提示

--- main.cpp
+++ main.cpp
@@ -2,21 +2,26 @@
 #include <vector>
 using namespace std;
 void gray(int n, vector<string>& list){
+    //终止条件
     if (n == 1){
         list[0] = "0";
         list[1] = "1";
         return;
     }
 
+    //递归
     gray(n-1,list);
 
+    //生成下方对称数组
     for (int i = pow(2,n-1); i < pow(2, n); ++i) {
         list[i] = list[pow(2,n)-1-i];
     }
 
+    //上半部分球迷添加0
     for (int i = 0; i < pow(2, n - 1); ++i) {
         list[i] = "0"+list[i];
     }
+    //下半部分前面添加1
     for (int i = pow(2, n - 1); i < pow(2, n); ++i) {
         list[i] = "1"+list[i];
     }
@@ -24,6 +29,7 @@ void gray(int n, vector<string>& list){
 
 int main(){
     int n;
+    cout<<"请输入格雷码位数:";
     cin >> n;
     vector<string> list(pow(2,n));
 
